8d1bd05a94d5e782390e9cfb58a0ebf224278588
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2022-06-22 18:35:44 +0300

    небольшие правки

--- js/thumbnail-rendering.js
+++ js/thumbnail-rendering.js
@@ -1,21 +1,18 @@
 import { getSimilarPhotosDescriptions } from './data.js';
 
 const similarPhotos = getSimilarPhotosDescriptions();
-console.log(similarPhotos);
 const containerImagesOtherUsers = document.querySelector('.pictures');
-
-
 const pictureUserTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
 const photosListFragment = document.createDocumentFragment();
 
-similarPhotos.forEach((similarPhoto) => {
+similarPhotos.forEach(({ url, likes, comments }) => {
   const pictureUserElement = pictureUserTemplate.cloneNode(true);
-  pictureUserElement.querySelector('.picture__img').src = similarPhoto.url;
-  pictureUserElement.querySelector('.picture__likes').textContent = similarPhoto.likes;
-  pictureUserElement.querySelector('.picture__comments').textContent = similarPhoto.comments.length;
+  pictureUserElement.querySelector('.picture__img').src = url;
+  pictureUserElement.querySelector('.picture__likes').textContent = likes;
+  pictureUserElement.querySelector('.picture__comments').textContent = comments.length;
   photosListFragment.append(pictureUserElement);
 });
 
