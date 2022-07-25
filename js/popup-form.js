2439052fb2b2a55aaccdf82064d73f6403b032f8
Artem Salnikov <88932070+wunschpunsh@users.noreply.github.com>
2022-07-25 17:30:42 +0300

    правки для защиты проекта

--- js/popup-form.js
+++ js/popup-form.js
@@ -1,4 +1,4 @@
-import {onChangeBiggerScale, onChangeSmallerScale,scaleBiggerButton,scaleSmallerButton} from './image-editor.js';
+import {onChangeBiggerScaleClick, onChangeSmallerScaleClick,scaleBiggerButton,scaleSmallerButton, setDefaultState} from './image-editor.js';
 
 const imageUploadForm = document.querySelector('.img-upload__form');
 const loadUserPhoto = imageUploadForm.querySelector('#upload-file');
@@ -9,10 +9,12 @@ const textHashtags = imageUploadForm.querySelector('.text__hashtags');
 const textDescription = imageUploadForm.querySelector('.text__description');
 const submitButton = imageUploadForm.querySelector('.img-upload__submit');
 
+let closeForm = null;
 const showPreview = () => {
   const file = loadUserPhoto.files[0];
   imgUploadPreview.src = URL.createObjectURL(file);
 };
+
 
 const blockSubmitButton = () => {
   submitButton.disabled = true;
@@ -24,38 +26,46 @@ const unblockSubmitButton = () => {
   submitButton.textContent = 'Опубликовать';
 };
 
+const onCloseImageUploadFormClick = () => {
+  closeForm();
+};
+
 const onPopupKeyDown = (evt) => {
   if (evt.key === 'Escape') {
     evt.preventDefault();
     imageEditor.classList.add('hidden');
     document.body.classList.remove('modal-open');
     document.removeEventListener('keydown', onPopupKeyDown);
+    closeImageUploadForm.removeEventListener('click', onCloseImageUploadFormClick);
+    scaleBiggerButton.removeEventListener('click', onChangeBiggerScaleClick);
+    scaleSmallerButton.removeEventListener('click', onChangeSmallerScaleClick);
   }
 };
+
 
 const openForm = () => {
   imageEditor.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onPopupKeyDown);
-  scaleSmallerButton.addEventListener('click', onChangeSmallerScale);
-  scaleBiggerButton.addEventListener('click', onChangeBiggerScale);
+  scaleSmallerButton.addEventListener('click', onChangeSmallerScaleClick);
+  scaleBiggerButton.addEventListener('click', onChangeBiggerScaleClick);
 };
 
-const closeForm = () => {
+closeForm = () => {
   imageEditor.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupKeyDown);
-  scaleBiggerButton.removeEventListener('click', onChangeBiggerScale);
-  scaleSmallerButton.removeEventListener('click', onChangeSmallerScale);
+  scaleBiggerButton.removeEventListener('click', onChangeBiggerScaleClick);
+  scaleSmallerButton.removeEventListener('click', onChangeSmallerScaleClick);
+  closeImageUploadForm.removeEventListener('click', onCloseImageUploadFormClick);
   imageUploadForm.reset();
+  setDefaultState();
 };
 
 loadUserPhoto.addEventListener('change', () => {
   openForm();
   showPreview();
-  closeImageUploadForm.addEventListener('click', () => {
-    closeForm();
-  });
+  closeImageUploadForm.addEventListener('click', onCloseImageUploadFormClick);
 });
 
 
--- js/result-message.js
+++ js/result-message.js
@@ -6,23 +6,36 @@ const getSuccesMessage = () => {
   const succsesMessage = succsesMessageTemplate.cloneNode(true);
   const successButton = succsesMessage.querySelector('.success__button');
   document.body.appendChild(succsesMessage);
+  let onSuccesMessageOutsideClick = null;
+
   const onSuccesMessageKeyDown = (evt) => {
     if (evt.key === 'Escape') {
       evt.preventDefault();
       succsesMessage.classList.add('hidden');
       document.removeEventListener('keydown', onSuccesMessageKeyDown);
+      document.removeEventListener('click', onSuccesMessageOutsideClick);
     }
+
   };
-  const onSuccesMessageOutsideClick = (evt) => {
+
+  onSuccesMessageOutsideClick = (evt) => {
 
     if(succsesMessage === evt.target) {
       succsesMessage.classList.add('hidden');
       document.removeEventListener('click', onSuccesMessageOutsideClick);
+      document.removeEventListener('keydown', onSuccesMessageKeyDown);
     }
+
   };
-  successButton.addEventListener('click', ()=> {
+
+  const onSuccessButtonClick = () => {
     succsesMessage.classList.add('hidden');
-  });
+    successButton.removeEventListener('click', onSuccessButtonClick);
+    document.removeEventListener('click', onSuccesMessageOutsideClick);
+    document.removeEventListener('keydown', onSuccesMessageKeyDown);
+  };
+
+  successButton.addEventListener('click', onSuccessButtonClick);
   document.addEventListener('keydown', onSuccesMessageKeyDown);
   document.addEventListener('click', onSuccesMessageOutsideClick);
 };
@@ -36,22 +49,35 @@ const getErrorMessage = () => {
   const errorButton = errorMessage.querySelector('.error__button');
   errorMessage.style.zIndex = '100';
   document.body.appendChild(errorMessage);
+  let onErrorMessageOutsideClick = null;
+
   const onErrorMessageKeyDown = (evt) => {
     if (evt.key === 'Escape') {
       evt.preventDefault();
       errorMessage.classList.add('hidden');
       document.removeEventListener('keydown', onErrorMessageKeyDown);
+      document.removeEventListener('click', onErrorMessageOutsideClick);
     }
+
   };
-  const onErrorMessageOutsideClick = (evt) => {
+
+  onErrorMessageOutsideClick = (evt) => {
     if(errorMessage === evt.target) {
       errorMessage.classList.add('hidden');
       document.removeEventListener('click', onErrorMessageOutsideClick);
+      document.removeEventListener('keydown', onErrorMessageKeyDown);
     }
+
   };
-  errorButton.addEventListener('click', ()=> {
+
+  const onErrorButtonClick = () => {
     errorMessage.classList.add('hidden');
-  });
+    errorButton.removeEventListener('click', onErrorButtonClick);
+    document.removeEventListener('click', onErrorMessageOutsideClick);
+    document.removeEventListener('keydown', onErrorMessageKeyDown);
+  };
+
+  errorButton.addEventListener('click', onErrorButtonClick);
   document.addEventListener('keydown', onErrorMessageKeyDown);
   document.addEventListener('click', onErrorMessageOutsideClick);
 };
